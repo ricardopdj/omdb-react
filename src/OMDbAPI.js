aeb64e89c48ff488afb391d24b7ac7d65969ccb2
Ricardo <pareja.r@gmail.com>
2019-01-13 03:59:38 -0200

    Test https omdb

--- src/OMDbAPI.js
+++ src/OMDbAPI.js
@@ -1,4 +1,4 @@
-const api = "http://www.omdbapi.com/?apikey=576f8801";
+const api = "https://www.omdbapi.com/?apikey=576f8801";
 
 export const searchByTitle = (title, page) => fetch(`${api}&s=${title}&page=${page}`)
     .then(res => res.json())
