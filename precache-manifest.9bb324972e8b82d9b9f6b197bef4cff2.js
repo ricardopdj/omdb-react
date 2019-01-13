51b3b1ae1dd23d0a433978f2b92c35f6f431e99f
Ricardo <pareja.r@gmail.com>
2019-01-13 03:51:49 -0200

    Updates

---
+++ precache-manifest.9bb324972e8b82d9b9f6b197bef4cff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "ricardopdj.github.io/omdb-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53698ed0429b2c95b025",
+    "url": "ricardopdj.github.io/omdb-react/static/js/runtime~main.53698ed0.js"
+  },
+  {
+    "revision": "47b77aceace792277ec2",
+    "url": "ricardopdj.github.io/omdb-react/static/js/main.47b77ace.chunk.js"
+  },
+  {
+    "revision": "3e10669722cf1058520b",
+    "url": "ricardopdj.github.io/omdb-react/static/js/1.3e106697.chunk.js"
+  },
+  {
+    "revision": "47b77aceace792277ec2",
+    "url": "ricardopdj.github.io/omdb-react/static/css/main.902f54ed.chunk.css"
+  },
+  {
+    "revision": "3e10669722cf1058520b",
+    "url": "ricardopdj.github.io/omdb-react/static/css/1.83f8381d.chunk.css"
+  },
+  {
+    "revision": "f86b2826f65ec36b8e04a14691d5163f",
+    "url": "ricardopdj.github.io/omdb-react/index.html"
+  }
+];
